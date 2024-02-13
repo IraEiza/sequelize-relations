d0fa8ad9ef829df6937c304923e8791fc2823e8d
IraEiza <iratze.eizaguirre@example.com>
2024-02-13 19:38:23 +0000

    reset

--- db/index.js
+++ db/index.js
@@ -6,9 +6,6 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   dialect: 'mysql',
   port: 3306,
   logging: false,
-  define: {
-    timestamps: false
-  }
 });
 
 
@@ -23,7 +20,7 @@ async function checkDB() {
 
 async function syncModels () {
   try {
-    await sequelize.sync({alter: true})
+    await sequelize.sync()
     console.log('Models Synchronized!')
   } catch (error) {
     console.log(error)
